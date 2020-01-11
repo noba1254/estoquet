94de8aba8db0fedfb760874e4f9249e5a5f45d96
noba1254 <raaschraphael@gmail.com>
2020-01-10 23:12:26 -0300

    First commit

--- djangosige/configs/settings.py
+++ djangosige/configs/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=[], cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default=['127.0.0.1', 'app-pastel.herokuapp.com'], cast=Csv())
 
 if not DEFAULT_DATABASE_URL:
     DEFAULT_DATABASE_URL = 'sqlite:///' + os.path.join(APP_ROOT, 'db.sqlite3')
